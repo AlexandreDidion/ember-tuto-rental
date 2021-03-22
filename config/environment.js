60c8c2b2a725ab92b621d399e9aa12fad47c2ccb
AlexandreDidion <ale.didion@gmail.com>
2021-03-22 15:47:51 +0100

    Finish first part ember tuto

--- config/environment.js
+++ config/environment.js
@@ -47,5 +47,7 @@ module.exports = function (environment) {
     // here you can enable a production-specific feature
   }
 
+  ENV.MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoiYWxlZGlkIiwiYSI6ImNraTZhcDN6NzE3MTQyc3FzcDFkbDBibnUifQ.xErfmmqfw2-Dur2mVuTakQ';
+
   return ENV;
 };
